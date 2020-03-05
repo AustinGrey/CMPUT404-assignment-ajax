e149643663c3e2249bca0031657921879e4d8a27
AustinGrey <corey.crapo@gmail.com>
2020-03-04 21:56:51 -0700

    Changed to use queues instead of sending the whole world all at once

--- server.py
+++ server.py
@@ -36,24 +36,63 @@ app.debug = True
 
 class World:
     def __init__(self):
+        self.space = dict()
+
+        # A dictionary of updates. Key is client provided id (yes I am aware this isn't secure,
+        # it's a drawing app), values are dictionaries of entity updates, a subset of world.
+        self.queues = dict()
+
         self.clear()
 
     def update(self, entity, key, value):
-        entry = self.space.get(entity,dict())
+        entry = self.space.get(entity, dict())
         entry[key] = value
         self.space[entity] = entry
 
     def set(self, entity, data):
+        """
+        Sets the given entity to a specific value.
+        Informs all queues of the update
+        """
         self.space[entity] = data
+
+        # Append this update to every queue
+        for queue in self.queues:
+            self.queues[queue][entity] = data
+
+        # for queue in self.queues:
+        #     queue[entity] = data
+
+    # def pop_queue(self, queue):
+    #     """
+    #     Gets the specified queue, then clears it
+    #     """
+    #     for
 
     def clear(self):
         self.space = dict()
+        self.queues = dict()
 
     def get(self, entity):
-        return self.space.get(entity,dict())
+        return self.space.get(entity, dict())
 
     def world(self):
         return self.space
+
+    def queue(self, queue_id):
+        if queue_id in self.queues:
+            result = self.queues[queue_id]
+            self.queues[queue_id] = dict()
+            return result
+        else:
+            return dict()
+
+    def register_queue(self, queue_id):
+        """
+        Create a queue with the given queue_id
+        """
+        if queue_id not in self.queues:
+            self.queues[queue_id] = dict()
 
 # you can test your webservice from the commandline
 # curl -v   -H "Content-Type: application/json" -X PUT http://127.0.0.1:5000/entity/X -d '{"x":1,"y":1}'
@@ -80,7 +119,11 @@ def hello():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    myWorld.set(entity, flask_post_json())
+    post_vars = flask_post_json()
+    if 'uuid' in post_vars:
+        myWorld.register_queue(post_vars['uuid'])
+
+    myWorld.set(entity, post_vars)
     return jsonify(myWorld.get(entity))
 
 @app.route("/world", methods=['POST','GET'])
@@ -98,6 +141,21 @@ def clear():
     '''Clear the world out!'''
     myWorld.clear()
     return jsonify(myWorld.world())
+
+@app.route("/get_updates/<queue_id>", methods=['POST', 'GET'])
+def get_queue(queue_id):
+    '''
+    Get the queue of updates waiting for this specific id
+    Also clears the queue
+    '''
+    return jsonify(myWorld.queue(queue_id))
+
+# @app.route('/register/<queue_id>', methods=['GET'])
+# def register_queue(queue_id):
+#     """
+#     Registers a queue for this id in the system
+#     """
+#     myWorld.register_queue(queue_id)
 
 if __name__ == "__main__":
     app.run()
